48f03377f09b5d817d35bbc46191d0548041ccf4
4r2awy <abdelrahamnshark2025@gmail.com>
2026-09-16 18:03:31 +0300

    - init FitFlow

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(FitFlow());
+}
+
+class FitFlow extends StatelessWidget {
+  const FitFlow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
